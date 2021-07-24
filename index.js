c7b58d567e3fc020036390e5799ac0b7e4ac40d4
A.Karwowski <akk42@zips.uakron.edu>
2021-07-23 21:43:02 -0400

    add a couple basic commands

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const config = require('./config.json')
+const command = require('./util/command')
+
+
+//==========//End Imports//==========//
 
 client.on('ready', () => {
     console.log('The client is ready')
+
+    command(client, ['ping', 'test'], (message) => {
+        message.channel.send('Pong!')
+    })
+    command(client, 'servers', message => {
+        client.guilds.cache.forEach((guild) => {
+            message.channel.send(`${guild.name} has a total of ${guild.memberCount} members.`)
+        })
+    })
+    command(client, ['cc', 'clearchannel'], message => {
+        if(message.member.hasPermission('ADMINISTRATOR')) {
+            message.channel.messages.fetch().then((results) => {
+                message.channel.bulkDelete(results)
+            })
+        }
+    })
 })
 
 client.login(config.token)
