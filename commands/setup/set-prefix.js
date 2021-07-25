d6c2179b4b467b9a257284119c20d09052c25707
A.Karwowski <akk42@zips.uakron.edu>
2021-07-24 22:55:54 -0400

    set server prefixes

---
+++ commands/setup/set-prefix.js
@@ -0,0 +1,26 @@
+const mongo = require('../../util/mongoose')
+const commandPrefix = require('../../schemas/command-prefix')
+const commandBase = require('../command-base')
+module.exports = {
+    commands: 'setprefix',
+    minArgs: 1,
+    maxArgs: 1,
+    expectedArgs: "<This bot's new command prefix>",
+    permissionError: "You must be an admin to run this command.",
+    permissions: 'ADMINISTRATOR',
+    callback: async (message, arguments, text) => {
+        await mongo().then(async mongoose => {
+            try {
+                const _id = message.guild.id
+                const prefix = arguments[0]
+
+                await commandPrefix.findByIdAndUpdate({_id}, {_id, prefix}, {upsert:true})
+
+                message.reply(`The prefix for this bot is now ${prefix}`)
+                commandBase.updateCache(_id, prefix)
+            } finally {
+                mongoose.connection.close()
+            }
+        })
+    }
+}
