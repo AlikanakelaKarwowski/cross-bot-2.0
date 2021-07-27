4af0bfdb32c068d1c51dd3874d23ee76204d1db2
A.Karwowski <akk42@zips.uakron.edu>
2021-07-27 12:20:56 -0400

    add notification

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const config = require('./config.json')
-
-
+const mongo = require('./util/mongoose')
+const banList = require('./schemas/banList')
 //==========//End Imports//==========//
 
 client.on('ready', async () => {
@@ -25,11 +25,54 @@ client.on('ready', async () => {
             }
         }
     }
-    
+
     readCommands('commands')
     commandBase.listen(client)
     commandBase.loadPrefixes(client)
     
+    client.on("guildMemberAdd", async (member) => {
+        console.log("searching for bans");
+        
+        const con = await mongo();
+        try {
+            const bans = await banList.find({Discord_id : member.id});
+            if (!bans) {
+                return;
+            } else {
+                console.log("creating ban message");
+                const embed = new Discord.MessageEmbed()
+                    .setTitle(`Warning ${bans[0].User} was previously banned `)
+                    .setAuthor(bans[0].Moderator)
+                    .setFooter(`ID:${bans[0].Discord_id}\t Cross Bot 2.0`)
+                    .setColor("#FFOOOO")
+                    .addFields(
+                        {
+                            name: "User",
+                            value: `${bans[0].User}`,
+                            inline: true,
+                        },
+                        {
+                            name: "Reason",
+                            value: bans[0].Reason,
+                            inline: true,
+                        },
+                        {
+                            name: "Moderator",
+                            value: bans[0].Moderator,
+                            inline: true,
+                        }
+                    );
+                client.channels.cache.get("868293138421284968").send(embed);
+            }
+        } catch (err) {
+            console.log(err);
+        } finally {
+            con.connection.close();
+        }
+        
+    });
+
+    client.emit("guildMemeberAdd", client.user);
 })
 
 client.login(config.token)
