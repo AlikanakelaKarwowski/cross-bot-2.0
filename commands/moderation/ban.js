d719edd49d4ecb52c5c3c6554e211e1bd560feb2
A.Karwowski <akk42@zips.uakron.edu>
2021-07-26 21:43:14 -0400

    fix ban stuff

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,33 +1,68 @@
 const mongo = require('../../util/mongoose')
-const banlist = require('../../schemas/banList')
+const Discord = require('discord.js')
 const banList = require('../../schemas/banList')
 module.exports = {
     commands:'ban',
     minArgs: 2,
-    expectedArgs: "<Targer users ID> <reason>",
+    expectedArgs: "<Users ID or @> <reason>",
     requiredRoles: [],
     callback: async (message, arguments) => {
         const target = message.mentions.users.first()
         
         if(target) {
+            await mongo()
+            const Discord_id = target.id
+            const User = target.username || "N/A"
+            arguments.shift()
+            const Reason = arguments.join(' ')
+            
+            const Moderator = message.author.username
+            const Server = message.guild.name
+            const ban = new banList({
+                Discord_id,
+                User,
+                Reason,
+                Moderator,
+                Server,
+            })
+
             try{
-                const mdb = await mongo()
-                console.log(mdb)
+                await ban.save()  
             }catch (err) {
                 console.log(err)
-            }finally {
-                
             }
-            message.channel.send(`User <@${target.id}> was banned`)
+            
+            const embed = new Discord.MessageEmbed()
+            .setTitle(`${User} Banned`)
+            .setAuthor(Moderator)
+            .setFooter(`ID:${Discord_id}\t Cross Bot 2.0`)
+            .setColor('#FFOOOO')
+            .addFields(
+                {
+                    name: "User",
+                    value: `${User}`,
+                    inline: true
+                },
+                {
+                    name: "Reason",
+                    value: Reason,
+                    inline: true
+                },
+                {
+                    name: "Moderator",
+                    value: Moderator,
+                    inline: true
+                }   
+            )
+            message.channel.send(embed)
 
 
         } else {
+            await mongo()
             const Discord_id = arguments[0]
             const member= await message.guild.members.fetch()
             
             if(member.get(Discord_id)) {
-                
-                const mdb = await mongo()
                 const User = member.get(Discord_id).user.username
                 
                 arguments.shift()
@@ -44,16 +79,32 @@ module.exports = {
                 })
                 try{
                     await ban.save()
-
-                    
                 }catch (err) {
                     console.log(err)
-                }finally {
-                    
                 }
-                
-                
-                message.channel.send(`User <@${Discord_id}> was banned`)
+                const embed = new Discord.MessageEmbed()
+                .setTitle(`${User} Banned`)
+                .setAuthor(Moderator)
+                .setFooter(`ID:${Discord_id}\t Cross Bot 2.0`)
+                .setColor('#FFOOOO')
+                .addFields(
+                    {
+                        name: "User",
+                        value: `${User}`,
+                        inline: true
+                    },
+                    {
+                        name: "Reason",
+                        value: Reason,
+                        inline: true
+                    },
+                    {
+                        name: "Moderator",
+                        value: Moderator,
+                        inline: true
+                    }   
+                )
+                message.channel.send(embed)
             } else {
                 message.channel.send(`Not a valid ID or mention`)
             }  
