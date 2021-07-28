43a6fb5633300b16b355662c6b5249b716fba207
A.Karwowski <akk42@zips.uakron.edu>
2021-07-27 23:28:33 -0400

    remove files that are no longer being used

--- commands/util/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    commands: 'ping',
-    minArgs: 0,
-    maxArgs: 0,
-    callback: (message, arguments, text) => {
-        const ping = Math.floor((message.createdTimestamp - Math.floor(Date.now()))/10)
-        message.channel.send(`Pong ${ping}ms`)
-    }
-}
