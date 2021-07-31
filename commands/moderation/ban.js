c964adf62ec55e2ef2735e29ec2ea7b769fd92cb
A.Karwowski <akk42@zips.uakron.edu>
2021-07-31 17:07:10 -0400

    jank together validation while i wait for an answer

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -2,6 +2,7 @@ const Commando = require('discord.js-commando')
 const banList = require('../../schemas/banList')
 const Discord = require('discord.js')
 const roleList = require('../../schemas/roleList')
+
 module.exports = class BanCommand extends Commando.Command {
     constructor(client) {
         super(client, {
@@ -15,67 +16,83 @@ module.exports = class BanCommand extends Commando.Command {
         });
     }
 
-    hasPermission(message) {
+    async hasPermission(message) {
         // TODO: Create a command to set guild roles for banning people.
-        const roles = await roleList.find({Guild_id: message.guild.id})
-        return (message.member.roles.cache.some(role => roles.includes(role)))
+        
+        // roleList.find({Guild_id: message.guild.id}).then(roles => {
+        //     console.log(roles[0].Roles)
+        //     const b= (message.member.roles.cache.some(role => roles[0].Roles.includes(role.name)))
+        //     console.log("Has permission?", b)
+        //     return b
+        // })
+        
+        
+        
     }
 
     async run(message, args) {
-        var Discord_id = args[0];
-        if (Discord_id.startsWith("<@")) {
-            Discord_id = Discord_id.match(/\d{17,18}/)[0];
-        } else if (isNaN(Discord_id) || !Discord_id) {
-            message.channel.send("You probably typed something in wrong.");
-            return;
-        }
-       
-        const member = await message.guild.members.fetch(Discord_id);
-
-        if (member) {
-            const User = member.user.username || "N/A";
-            args.shift();
-            const Reason = args.join(" ");
-            const Moderator = message.author.username;
-            const Server = member.guild.name || "N/A";
-            const ban = new banList({
-                Discord_id,
-                User,
-                Reason,
-                Moderator,
-                Server,
-            });
-            try {
-                await ban.save();
-            } catch (err) {
-                console.log(err);
+        const perm = await roleList.find({ Guild_id: message.guild.id });
+        console.log(perm[0].Roles);
+        const hasPerm =  message.member.roles.cache.some((role) => perm[0].Roles.includes(role.name));
+        if(hasPerm) {
+            var Discord_id = args[0];
+            if (Discord_id.startsWith("<@")) {
+                Discord_id = Discord_id.match(/\d{17,18}/)[0];
+            } else if (isNaN(Discord_id) || !Discord_id) {
+                message.channel.send("You probably typed something in wrong.");
+                return;
             }
-
-            const embed = new Discord.MessageEmbed()
-                .setTitle(`${User} Banned`)
-                .setAuthor(Moderator)
-                .setFooter(`ID:${Discord_id}\t Cross Bot 2.0`)
-                .setColor("#FFOOOO")
-                .addFields(
-                    {
-                        name: "User",
-                        value: `${User}`,
-                        inline: true,
-                    },
-                    {
-                        name: "Reason",
-                        value: Reason,
-                        inline: true,
-                    },
-                    {
-                        name: "Moderator",
-                        value: Moderator,
-                        inline: true,
-                    }
-                );
-            message.channel.send(embed);
+           
+            const member = await message.guild.members.fetch(Discord_id);
+    
+            if (member) {
+                const User = member.user.username || "N/A";
+                args.shift();
+                const Reason = args.join(" ");
+                const Moderator = message.author.username;
+                const Server = member.guild.name || "N/A";
+                const ban = new banList({
+                    Discord_id,
+                    User,
+                    Reason,
+                    Moderator,
+                    Server,
+                });
+                try {
+                    await ban.save();
+                } catch (err) {
+                    console.log(err);
+                }
+    
+                const embed = new Discord.MessageEmbed()
+                    .setTitle(`${User} Banned`)
+                    .setAuthor(Moderator)
+                    .setFooter(`ID:${Discord_id}\t Cross Bot 2.0`)
+                    .setColor("#FFOOOO")
+                    .addFields(
+                        {
+                            name: "User",
+                            value: `${User}`,
+                            inline: true,
+                        },
+                        {
+                            name: "Reason",
+                            value: Reason,
+                            inline: true,
+                        },
+                        {
+                            name: "Moderator",
+                            value: Moderator,
+                            inline: true,
+                        }
+                    );
+                message.channel.send(embed);
+            } else {
+                message.channel.send(`An Error has occured or discord is just being dumb`);
+            }
         } else {
-            message.channel.send(`An Error has occured or discord is just being dumb`);
+            message.channel.send(`<@${message.member.id}> You do not have permission to run the \`ban\` command.`)
         }
+
     }
 };
