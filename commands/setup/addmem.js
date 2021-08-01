09918eeefbe7ad5378273d3d4fd6e77f25483d37
A.Karwowski <akk42@zips.uakron.edu>
2021-08-01 18:31:10 -0400

    test adding a member

---
+++ commands/setup/addmem.js
@@ -0,0 +1,20 @@
+const Commando = require('discord.js-commando')
+
+module.exports = class addMemberCommand extends Commando.Command {
+    constructor(client){
+        super(client, {
+            name: 'addmem',
+            group: 'setup',
+            memberName: 'addmem',
+            description: 'Test notification of emit(\'guildMemberAdd\'',
+            argsType: 'single',
+            argsCount: 0,
+            guildOnly: true
+        })
+    }
+
+    async run(message) {
+        this.client.emit('guildMemberAdd', this.client.user)
+        this.client.emit('guildMemberAdd', message.member)
+    }
+}
