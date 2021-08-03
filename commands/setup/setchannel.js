091b58a7d7f8a1431b5ca1cd20faaf2388a9730e
Alex Karwowski <51871385+AlikanakelaKarwowski@users.noreply.github.com>
2021-08-03 10:38:13 -0400

    finish basics of  setchannels command

--- commands/setup/setchannel.js
+++ commands/setup/setchannel.js
@@ -6,7 +6,29 @@ module.exports = class setChannelCommand extends Commando.Command {
         super(client, {
             name: 'setchannel',
             group: 'setup',
-            memberName: 'setChannel'
+            memberName: 'setChannel',
+            description: 'Set Channel for notifications. Uses channel ID.',
+            argsType: 'multiple',
+            guildOnly: true,
+            examples: ["setchannel <Channel ID>"]
         })
+    }
+
+    hasPermission(message) {
+        return message.member.id === message.guild.ownerID
+    }
+
+    async run(message, args){
+        const Channel = args[1]
+        try{
+            await channnelList.updateOne(
+                {Guild_id: message.guild.id},
+                {Channel},
+                {upsert: true})
+        } catch (err) {
+            console.log(err)
+        }
+        updateChannels(this.client)
+        message.channel.cache.get(Channel).send(`I will send you notifications in this channel.`)
     }
 }
